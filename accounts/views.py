72930d35c1645e8b736ce3a6f5c4bd44a11a2931
Kwan Jeon <jeonkwan100@gmail.com>
2024-05-03 14:25:46 +0900

    accounts serializers add, views SignUpAPIView add

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,22 @@
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from django.contrib.auth.hashers import make_password
+from django.shortcuts import get_object_or_404
+
+from .models import User
+from .serializers import UserSerializer
 
 
 class SignUpAPIView(APIView):
-    pass
+    def post(self, request):
+        request.data['password'] = make_password(request.data['password'])
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ProfileAPIView(APIView):
