4866c9211f6384112c6f62f877b01b11827f8997
sjh <per_ma@naver.com>
2024-05-03 21:17:41 +0900

    crud생성

--- articles/views.py
+++ articles/views.py
@@ -4,11 +4,21 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated #로그인 인증토큰
 from .models import Article
 from .serializer import ArticleSerializer
 
 
+
 class ArticleListAPIView(APIView):
+    def get_permissions(self):#로그인 인증토큰
+        permissions = super().get_permissions()
+
+        if self.request.method.lower() == 'post': #포스트할때만 로그인
+            permissions.append(IsAuthenticated())
+
+        return permissions
+    
     def get(self, request):
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
@@ -18,10 +28,34 @@ class ArticleListAPIView(APIView):
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATE)
-        
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+class ArticleDetailAPIView(APIView):
+    def get_permissions(self): #로그인 인증토큰
+        permissions = super().get_permissions()
 
-        
-        
+        if self.request.method.lower() == ('put' or 'delete'):
+            permissions.append(IsAuthenticated())
 
-# Create your views here.
+        return permissions
+    
+    def get_object(self, pk):
+        return get_object_or_404(Article, pk=pk)
+
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleSerializer(article, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    def delete(self, request, pk):
+        article = self.get_object(pk)
+        article.delete()
+        data = {"pk": f"{pk} is deleted."}
+        return Response(data, status=status.HTTP_200_OK)
