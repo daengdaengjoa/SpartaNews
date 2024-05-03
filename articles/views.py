24981a5200cd4e80cb0cc2acbe3d55a0b50b42e0
sjh <per_ma@naver.com>
2024-05-03 17:29:30 +0900

    아티클 목록조회

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+class ArticleListAPIView(APIView):
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATE)
+        
+
+        
+        
 
 # Create your views here.
