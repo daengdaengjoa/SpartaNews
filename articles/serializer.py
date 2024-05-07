e1afdd65fc6297d41e81baa6af5848b98a01dad0
sjh <per_ma@naver.com>
2024-05-07 10:55:57 +0900

    로그인계정 작성수정가능,뷰카운트

--- articles/serializer.py
+++ articles/serializer.py
@@ -6,3 +6,6 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
+        extra_kwargs = {
+            'like_users': {'required': False}, #제외시켜주는함수
+        }
